8f02ce50b873fe529e29618354efcd59b535535c
ChanneDesign <channebertrand@gmail.com>
2016-07-01 12:31:21 -0400

    fixing mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
+    :domain         => 'saas-project-manager.herokuapp.com'
   }
 
   # Settings specified here will take precedence over those in config/application.rb.
